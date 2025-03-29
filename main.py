db227227ed1a5e1e95ba9d846b995f03b3e84256
Jacketdude <danedevilleres@gmail.com>
2025-03-29 21:29:37 +0700

    I Forgot to add the updated version of the main.py file

--- main.py
+++ main.py
@@ -14,17 +14,45 @@ class MyGame:
         self.bally = self.WINHEIGHT // 2
         self.ballrad = 15
 
-        self.paddle1x = 
+        # Common Paddle Properties
+        self.paddleSpeed = 10
+        self.paddleWidth = 15
+        self.paddleHeight = 90
+
+        #Paddle 1
+        self.paddle1x = 10
         self.paddle1y = self.WINHEIGHT // 2
 
+        # Paddle 2
+        self.paddle2x = self.WINWIDTH - 20
+        self.paddle2y = self.WINHEIGHT // 2
+
     def Update(self):
-        pass
+        # Paddle1 Movement
+        if is_key_down(KeyboardKey.KEY_W):
+            self.paddle1y -= self.paddleSpeed
+        
+        if is_key_down(KeyboardKey.KEY_S):
+            self.paddle1y += self.paddleSpeed
+
+        if is_key_down(KeyboardKey.KEY_UP):
+            self.paddle2y -= self.paddleSpeed
+
+        if is_key_down(KeyboardKey.KEY_DOWN):
+            self.paddle2y += self.paddleSpeed
+
+        # Yeah, Clamping Rocks!
+        self.paddle1y = int(clamp(float(self.paddle1y), 0, (self.WINHEIGHT - self.paddleHeight)))
+        self.paddle2y = int(clamp(float(self.paddle2y), 0, (self.WINHEIGHT - self.paddleHeight)))
+
 
     def Draw(self):
         begin_drawing()
-
+        
+        clear_background(BLACK)
         draw_circle(self.ballx, self.bally, self.ballrad, WHITE)
-        draw_rectangle()
+        draw_rectangle(self.paddle1x, self.paddle1y, self.paddleWidth, self.paddleHeight, WHITE)
+        draw_rectangle(self.paddle2x, self.paddle2y, self.paddleWidth, self.paddleHeight, WHITE)
 
         end_drawing()
 
