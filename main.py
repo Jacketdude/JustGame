cc2417ccc41d005befe5788fd9a3ad0f0baeccda
Jacketdude <danedevilleres@gmail.com>
2025-03-29 14:20:53 +0700

    Update 1

--- main.py
+++ main.py
@@ -13,14 +13,23 @@ class MyGame:
         self.playerSpeed = 20
 
     def Update(self):
+        begin_drawing()
+
+
         if (is_key_down(KeyboardKey.KEY_W)):
             self.PlayerY -= self.playerSpeed
+
         if (is_key_down(KeyboardKey.KEY_A)):
             self.PlayerX -= self.playerSpeed
+
         if (is_key_down(KeyboardKey.KEY_S)):
             self.PlayerY += self.playerSpeed
+
         if (is_key_down(KeyboardKey.KEY_D)):
             self.PlayerX += self.playerSpeed
+
+
+        end_drawing()
 
     def Draw(self):
         draw_circle(self.PlayerX, self.PlayerY, 20, WHITE)
@@ -28,10 +37,7 @@ class MyGame:
     def Run(self):
         while not window_should_close():
             self.Update()
-            begin_drawing()
-            clear_background(BLACK)
             self.Draw()
-            end_drawing()
         close_window()
 
 if __name__ == "__main__":
